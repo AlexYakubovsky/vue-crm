c0a65cf10d9fb17c9bd83df172781fd416df62c5
AlexYakubovsky <alex.yakubovsky.g@gmail.com>
2021-08-09 00:44:46 +0300

    Initial app

---
+++ src/registerServiceWorker.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+
+import { register } from "register-service-worker";
+
+if (process.env.NODE_ENV === "production") {
+    register(`${process.env.BASE_URL}service-worker.js`, {
+        ready() {
+            console.log(
+                "App is being served from cache by a service worker.\n" +
+                "For more details, visit https://goo.gl/AFskqB"
+            );
+        },
+        registered() {
+        },
+        cached() {
+        },
+        updatefound() {
+        },
+        updated() {
+        },
+        offline() {
+            console.log(
+                "No internet connection found. App is running in offline mode."
+            );
+        },
+        error(error) {
+            console.error("Error during service worker registration:", error);
+        }
+    });
+}
