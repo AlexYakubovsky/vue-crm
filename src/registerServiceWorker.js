21eccff56a1f19eb26edb0ea21a39eab8cbc3f69
AlexYakubovsky <alex.yakubovsky.g@gmail.com>
2021-08-09 22:29:28 +0300

    Add basic layout and router

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 
-import { register } from "register-service-worker";
+import {register} from "register-service-worker"
 
 if (process.env.NODE_ENV === "production") {
     register(`${process.env.BASE_URL}service-worker.js`, {
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === "production") {
             console.log(
                 "App is being served from cache by a service worker.\n" +
                 "For more details, visit https://goo.gl/AFskqB"
-            );
+            )
         },
         registered() {
         },
@@ -21,10 +21,10 @@ if (process.env.NODE_ENV === "production") {
         offline() {
             console.log(
                 "No internet connection found. App is running in offline mode."
-            );
+            )
         },
         error(error) {
-            console.error("Error during service worker registration:", error);
+            console.error("Error during service worker registration:", error)
         }
-    });
+    })
 }
--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,61 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 export default new VueRouter({
-    mode: "history",
+    mode: 'history',
     base: process.env.BASE_URL,
     routes: [
         {
             path: '/',
             name: 'Home',
             component: Home
+        },
+        {
+            path: '/categories',
+            name: 'Categories',
+            meta: {layout: 'main'},
+            component: () => import('../views/Categories')
+        },
+        {
+            path: '/detail-record',
+            name: 'DetailRecord',
+            meta: {layout: 'main'},
+            component: () => import('../views/DetailRecord')
+        },
+        {
+            path: '/history',
+            name: 'History',
+            meta: {layout: 'main'},
+            component: () => import('../views/History')
+        },
+        {
+            path: '/planning',
+            name: 'Planning',
+            meta: {layout: 'main'},
+            component: () => import('../views/Planning')
+        },
+        {
+            path: '/profile',
+            name: 'Profile',
+            meta: {layout: 'main'},
+            component: () => import('../views/Profile')
+        },
+        {
+            path: '/record',
+            name: 'Record',
+            meta: {layout: 'main'},
+            component: () => import('../views/Record')
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            meta: {layout: 'empty'},
+            component: () => import('../views/Login')
+        },
+        {
+            path: '/register',
+            name: 'Register',
+            meta: {layout: 'empty'},
+            component: () => import('../views/Register')
         }
     ]
 })
